091f52f2ee86106d750277ee7b740f5c6572fbc7
Lucas Ambrosio <lu.amb99@gmail.com>
2023-03-01 14:29:39 -0300

    nav list js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+function NavImgList() {
+  const tabmenu = document.querySelectorAll(".js-tabmenu li");
+  const tabcontent = document.querySelectorAll(".js-tabcontent section");
 
+  if (tabmenu.length && tabcontent.length) {
+    tabcontent[0].classList.add("show");
+
+    function showcontent(index) {
+      tabcontent.forEach((content) => {
+        content.classList.remove("show");
+      });
+      tabcontent[index].classList.add("show");
+    }
+
+    tabmenu.forEach((img, index) => {
+      img.addEventListener("click", () => {
+        showcontent(index);
+      });
+    });
+  }
+}
+NavImgList();
