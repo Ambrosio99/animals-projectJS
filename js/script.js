c57c9c573fd9b46b2e023cef6e3102f6f70ae412
Lucas Ambrosio <lu.amb99@gmail.com>
2023-04-25 16:36:38 -0300

    Modal Login

---
+++ js/script.js
@@ -0,0 +1,11 @@
+import scrollSuave from "./modules/scrollSuave.js";
+import animacaoScroll from "./modules/animacaoScroll.js";
+import initTabNav from "./modules/initTab.js";
+import accordion from "./modules/Accordion.js";
+import modal from "./modules/modal.js";
+
+modal();
+animacaoScroll();
+scrollSuave();
+initTabNav();
+accordion();
